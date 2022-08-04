05f02dfa4985f7afd05039c587474e6b8bc5e5ac
Sam Blundell <samcalblundell@googlemail.com>
2022-08-04 20:39:37 +0100

    refactor input handler to use object containing booleans rather than an array

--- inputHandler.js
+++ inputHandler.js
@@ -1,16 +1,23 @@
 export default class inputHandler {
     constructor(game) {
         this.game = game;
-        this.validKeys = ['ArrowRight', 'ArrowLeft', 'Space'];
-        this.currentInput = [];
-        window.addEventListener('keydown', key => {
-            if (this.validKeys.includes(key.code) && this.currentInput.includes(key.code) === false) {
-                this.currentInput.push(key.code);
+        this.keys = {
+            ArrowRight: false,
+            ArrowLeft: false,
+            Space: false,
+        };
+        this.validKeys = Object.keys(this.keys)
+        window.addEventListener('keydown', event => {
+            if (event.repeat === true) {
+                return;
+            }
+            if (this.validKeys.includes(event.code)) {
+                this.keys[event.code] = true;
             }
         })
-        window.addEventListener('keyup', key => {
-            if (this.validKeys.includes(key.code)) {
-                this.currentInput = this.currentInput.filter(key => key === key.code);
+        window.addEventListener('keyup', event => {
+            if (this.validKeys.includes(event.code)) {
+                this.keys[event.code] = false;
             }
         })
     }
